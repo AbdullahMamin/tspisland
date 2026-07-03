bd0f6ee147b8c14a2804cc09cacd64be33afd15b
Abdullah <aabdullahmamin@gmail.com>
2026-07-03 19:37:07 +0400

    Changed some things up and added function to send data unblocked

--- src/worker.c
+++ src/worker.c
@@ -5,6 +5,8 @@ static int g_argc;
 static char **g_argv;
 static int g_n_procs;
 static int g_rank;
+static int g_workers[MAX_WORKERS];
+static MPI_Request g_isend_requests[MAX_WORKERS];
 
 void InitWorkers(int *argc, char ***argv) {
     MPI_Init(argc, argv);
@@ -13,9 +15,25 @@ void InitWorkers(int *argc, char ***argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &g_n_procs);
     MPI_Comm_rank(MPI_COMM_WORLD, &g_rank);
     WorkerPrintf(ANY_RANK, "Hello!\n");
+    MasterDo(
+        if (g_n_procs > MAX_WORKERS) {
+            MasterPrintf("Too many workers!\n");
+            exit(EXIT_FAILURE);
+        }
+    )
+    for (int i = 0; i < g_n_procs; i++) {
+        g_workers[i] = i;
+    }
+    for (int i = 0; i < g_n_procs; i++) {
+        g_isend_requests[i] = MPI_REQUEST_NULL;
+    }
 }
 
 void DeinitWorkers(void) {
+    // Make sure all pending sends are finished
+    for (int i = 0; i < g_n_procs; i++) {
+        MPI_Wait(&g_isend_requests[i], MPI_STATUS_IGNORE);
+    }
     WorkerPrintf(ANY_RANK, "Goodbye!\n");
     MPI_Finalize();
 }
@@ -48,6 +66,21 @@ void WorkerSendU32(u32 *array, size n_elements, int dst_rank) {
     );
 }
 
+void WorkerISendU32(u32 *array, size n_elements, int dst_rank) {
+    assert(0 <= dst_rank && dst_rank < g_n_procs);
+    // Wait for previous send to finish (if it exists)
+    MPI_Wait(&g_isend_requests[dst_rank], MPI_STATUS_IGNORE);
+    MPI_Isend(
+        array,
+        n_elements,
+        MPI_UNSIGNED,
+        dst_rank,
+        0,
+        MPI_COMM_WORLD,
+        &g_isend_requests[dst_rank]
+    );
+}
+
 void WorkerReceiveU32(u32 *array, size n_elements, int src_rank) {
     assert(0 <= src_rank && src_rank < g_n_procs);
     MPI_Recv(
@@ -59,4 +92,8 @@ void WorkerReceiveU32(u32 *array, size n_elements, int src_rank) {
         MPI_COMM_WORLD,
         MPI_STATUS_IGNORE
     );
+}
+
+const int *AllWorkers(void) {
+    return g_workers;
 }
--- src/worker.h
+++ src/worker.h
@@ -4,7 +4,10 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 #include "types.h"
+
+#define MAX_WORKERS (1024)
 
 #define NONE_RANK (-2)
 #define ANY_RANK (-1)
@@ -13,7 +16,9 @@
 #define WorkerDo(rank, ...) {if ((rank) == ANY_RANK || WorkerRank() == (rank)) {__VA_ARGS__}}
 #define MasterDo(...) WorkerDo(MASTER_RANK, __VA_ARGS__)
 
-#define WorkerPrintf(rank, ...) WorkerDo((rank), printf("[%d]: ", (rank)); printf(__VA_ARGS__);)
+#define WorkersDo(ranks, n_ranks, ...) {for (int _r = 0; _r < (n_ranks); _r++) WorkerDo((ranks)[_r], __VA_ARGS__)}
+
+#define WorkerPrintf(rank, ...) WorkerDo((rank), printf("[%d]: ", WorkerRank()); printf(__VA_ARGS__);)
 #define MasterPrintf(...) WorkerPrintf(MASTER_RANK, __VA_ARGS__)
 
 // Initializes MPI and worker globals
@@ -37,7 +42,14 @@ int GetWorkerCount(void);
 // Send u32 array to another worker
 void WorkerSendU32(u32 *array, size n_elements, int dst_rank);
 
+// Send u32 array to another (non-blocking)
+// Note: Waits until last non-blocking send finished if there was one
+void WorkerISendU32(u32 *array, size n_elements, int dst_rank);
+
 // Receive u32 array from another worker
 void WorkerReceiveU32(u32 *array, size n_elements, int src_rank);
+
+// Returns array of all worker ranks
+const int *AllWorkers(void);
 
 #endif // WORKER_H
