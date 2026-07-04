7609aef34f67360ae900d52b1b7056e97aef7bce
Abdullah <aabdullahmamin@gmail.com>
2026-07-04 22:33:44 +0400

    Cleaning up a bit for refactor

--- src/worker.c
+++ src/worker.c
@@ -3,36 +3,31 @@
 // Worker globals
 static int g_argc;
 static char **g_argv;
-static int g_n_procs;
+static int g_n_workers;
 static int g_rank;
 static int g_workers[MAX_WORKERS];
-static MPI_Request g_isend_requests[MAX_WORKERS];
+static MPI_Request g_requests[MAX_WORKERS];
 
 void InitWorkers(int *argc, char ***argv) {
     MPI_Init(argc, argv);
     g_argc = *argc;
     g_argv = *argv;
-    MPI_Comm_size(MPI_COMM_WORLD, &g_n_procs);
+    MPI_Comm_size(MPI_COMM_WORLD, &g_n_workers);
     MPI_Comm_rank(MPI_COMM_WORLD, &g_rank);
-    WorkerPrintf(ANY_RANK, "Hello!\n");
-    MasterDo(
-        if (g_n_procs > MAX_WORKERS) {
-            MasterPrintf("Too many workers!\n");
-            exit(EXIT_FAILURE);
-        }
-    )
-    for (int i = 0; i < g_n_procs; i++) {
+    if (g_n_workers > MAX_WORKERS) {
+        MasterPanicf("Too many workers!\n");
+    }
+    for (int i = 0; i < g_n_workers; i++) {
         g_workers[i] = i;
     }
-    for (int i = 0; i < g_n_procs; i++) {
-        g_isend_requests[i] = MPI_REQUEST_NULL;
+    for (int i = 0; i < g_n_workers; i++) {
+        g_requests[i] = MPI_REQUEST_NULL;
     }
 }
 
 void DeinitWorkers(void) {
     // Make sure all pending sends are finished
-    WorkerWaitAllRequests();
-    WorkerPrintf(ANY_RANK, "Goodbye!\n");
+    WorkerWaitForAllRequests();
     MPI_Finalize();
 }
 
@@ -49,11 +44,11 @@ char **GetArgv(void) {
 }
 
 int GetWorkerCount(void) {
-    return g_n_procs;
+    return g_n_workers;
 }
 
 void WorkerSendU32(u32 *array, size n_elements, int dst_rank) {
-    assert(0 <= dst_rank && dst_rank < g_n_procs);
+    assert(0 <= dst_rank && dst_rank < g_n_workers);
     MPI_Send(
         array,
         n_elements,
@@ -65,9 +60,9 @@ void WorkerSendU32(u32 *array, size n_elements, int dst_rank) {
 }
 
 void WorkerISendU32(u32 *array, size n_elements, int dst_rank) {
-    assert(0 <= dst_rank && dst_rank < g_n_procs);
+    assert(0 <= dst_rank && dst_rank < g_n_workers);
     // Wait for previous send to finish (if it exists)
-    MPI_Wait(&g_isend_requests[dst_rank], MPI_STATUS_IGNORE);
+    MPI_Wait(&g_requests[dst_rank], MPI_STATUS_IGNORE);
     MPI_Isend(
         array,
         n_elements,
@@ -75,12 +70,12 @@ void WorkerISendU32(u32 *array, size n_elements, int dst_rank) {
         dst_rank,
         0,
         MPI_COMM_WORLD,
-        &g_isend_requests[dst_rank]
+        &g_requests[dst_rank]
     );
 }
 
 void WorkerReceiveU32(u32 *array, size n_elements, int src_rank) {
-    assert(0 <= src_rank && src_rank < g_n_procs);
+    assert(0 <= src_rank && src_rank < g_n_workers);
     MPI_Recv(
         array,
         n_elements,
@@ -92,12 +87,12 @@ void WorkerReceiveU32(u32 *array, size n_elements, int src_rank) {
     );
 }
 
-void WorkerWaitAllRequests(void) {
-    for (int i = 0; i < g_n_procs; i++) {
-        MPI_Wait(&g_isend_requests[i], MPI_STATUS_IGNORE);
+void WorkerWaitForAllRequests(void) {
+    for (int i = 0; i < g_n_workers; i++) {
+        MPI_Wait(&g_requests[i], MPI_STATUS_IGNORE);
     }
 }
 
-const int *AllWorkers(void) {
+const int *AllWorkerRanks(void) {
     return g_workers;
 }
--- src/worker.h
+++ src/worker.h
@@ -13,46 +13,51 @@
 #define ANY_RANK (-1)
 #define MASTER_RANK (0)
 
+// Macros to make specific worker do something
 #define WorkerDo(rank, ...) {if ((rank) == ANY_RANK || WorkerRank() == (rank)) {__VA_ARGS__}}
 #define MasterDo(...) WorkerDo(MASTER_RANK, __VA_ARGS__)
 
-#define WorkersDo(ranks, n_ranks, ...) {for (int _r = 0; _r < (n_ranks); _r++) WorkerDo((ranks)[_r], __VA_ARGS__)}
+// Macro to make a list of workers do something
+#define WorkersDo(ranks, n_ranks, ...) {for (i32 _r = 0; _r < (n_ranks); _r++) WorkerDo((ranks)[_r], __VA_ARGS__)}
 
+// Macros for making specific workers print something
 #define WorkerPrintf(rank, ...) WorkerDo((rank), printf("[%d]: ", WorkerRank()); printf(__VA_ARGS__);)
 #define MasterPrintf(...) WorkerPrintf(MASTER_RANK, __VA_ARGS__)
+#define WorkerPanicf(rank, ...) WorkerDo((rank), printf("[%d] PANIC: ", WorkerRank()); printf(__VA_ARGS__); exit(EXIT_FAILURE);)
+#define MasterPanicf(...) WorkerPanicf(MASTER_RANK, __VA_ARGS__)
 
 // Initializes MPI and worker globals
-void InitWorkers(int *argc, char ***argv);
+void InitWorkers(i32 *argc, char ***argv);
 
 // Closes MPI
 void DeinitWorkers(void);
 
-// Returns global worker rank
-int WorkerRank(void);
+// Returns current worker rank
+i32 WorkerRank(void);
 
 // Easy access to argc where needed
-int GetArgc(void);
+i32 GetArgc(void);
 
 // Easy access to argv where needed
 char **GetArgv(void);
 
-// Returns n_procs
-int GetWorkerCount(void);
+// Returns number of workers available
+i32 WorkerCount(void);
 
 // Send u32 array to another worker
-void WorkerSendU32(u32 *array, size n_elements, int dst_rank);
+void WorkerSendU32(u32 *array, size n_elements, i32 dst_rank);
 
 // Send u32 array to another (non-blocking)
 // Note: Waits until last non-blocking send finished if there was one
-void WorkerISendU32(u32 *array, size n_elements, int dst_rank);
+void WorkerISendU32(u32 *array, size n_elements, i32 dst_rank);
 
 // Receive u32 array from another worker
-void WorkerReceiveU32(u32 *array, size n_elements, int src_rank);
+void WorkerReceiveU32(u32 *array, size n_elements, i32 src_rank);
 
 // Waits for all pending requests to finish.
-void WorkerWaitAllRequests(void);
+void WorkerWaitForAllRequests(void);
 
 // Returns array of all worker ranks
-const int *AllWorkers(void);
+const i32 *AllWorkerRanks(void);
 
 #endif // WORKER_H
