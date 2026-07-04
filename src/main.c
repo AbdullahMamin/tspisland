57331fd5375009c9731d0ae977751a5d22c95215
Abdullah <aabdullahmamin@gmail.com>
2026-07-05 01:32:33 +0400

    Removed voids

--- src/main.c
+++ src/main.c
@@ -1,8 +1,6 @@
 #include "worker.h"
 
 i32 main(i32 argc, char *argv[]) {
-    (void)argc;
-    (void)argv;
     InitWorkers(&argc, &argv);
     WorkerPrintf(ANY_RANK, "Hello!\n");
     DeinitWorkers();
