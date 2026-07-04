1704a91a95499f17a2ab7c1ad3a59648c59f74b0
Abdullah <aabdullahmamin@gmail.com>
2026-07-04 23:14:21 +0400

    Cleaned up util

--- src/util.c
+++ src/util.c
@@ -4,19 +4,13 @@ void SeedRNG(void) {
     srand(time(NULL));
 }
 
-u32 RandomU32(u32 min, u32 max) {
+i32 RandomInt(i32 min, i32 max) {
     assert(max >= min);
     if (min == max) return min;
     return min + rand()%(max - min + 1);
 }
 
-i32 RandomI32(i32 min, i32 max) {
-    assert(max >= min);
-    if (min == max) return min;
-    return min + rand()%(max - min + 1);
-}
-
-f64 RandomF64(f64 min, f64 max) {
+f64 RandomFloat(f64 min, f64 max) {
     assert(max >= min);
     return min + ((f64)rand()/(f64)RAND_MAX)*(max - min);
 }
@@ -24,7 +18,7 @@ f64 RandomF64(f64 min, f64 max) {
 bool CoinFlip(f64 chance) {
     assert(0.0 <= chance && chance <= 1.0);
     if (chance == 0.0) return false;
-    return RandomF64(0.0, 1.0) <= chance;
+    return RandomFloat(0.0, 1.0) <= chance;
 }
 
 // TODO: this algorithm is slow, but should be fine for our purposes
@@ -43,46 +37,6 @@ bool IsPrime(u32 x) {
     }
 
     return true;
-}
-
-// Fihser-Yates shuffling algorithm
-void ShuffleArrayU32(u32 *array, u32 n_elements, u32 i, u32 j) {
-    assert(array && i < n_elements && j < n_elements);
-
-    u32 length = (i <= j) ? (j - i + 1) : (n_elements - j + i + 1);
-    if (length <= 1) {
-        return;
-    }
-
-    for (u32 a = 0; a <= length - 2; a++) {
-        u32 b = RandomU32(a, length - 1);
-        u32 idx1 = (i + a)%n_elements;
-        u32 idx2 = (i + b)%n_elements;
-        u32 temp = array[idx1];
-        array[idx1] = array[idx2];
-        array[idx2] = temp;
-    }
-}
-
-void ReverseArrayU32(u32 *array, u32 n_elements, u32 i, u32 j) {
-    assert(array && i < n_elements && j < n_elements);
-
-    u32 length = (i <= j) ? (j - i + 1) : (n_elements - j + i + 1);
-    if (length <= 1) {
-        return;
-    }
-
-    for (u32 a = 0; a < length/2; a++) {
-        u32 temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-        i = (i + 1)%n_elements;
-        if (j == 0) {
-            j = n_elements - 1;
-        } else {
-            j--;
-        }
-    }
 }
 
 i32 RoundNearest(f64 x) {
--- src/util.h
+++ src/util.h
@@ -9,21 +9,13 @@
 
 void SeedRNG(void);
 
-u32 RandomU32(u32 min, u32 max);
+i32 RandomInt(i32 min, i32 max);
 
-i32 RandomI32(i32 min, i32 max);
-
-f64 RandomF64(f64 min, f64 max);
+f64 RandomFloat(f64 min, f64 max);
 
 bool CoinFlip(f64 chance);
 
 bool IsPrime(u32 x);
-
-// Shuffle array of u32s from i to j (i and j can wrap around)
-void ShuffleArrayU32(u32 *array, u32 n_elements, u32 i, u32 j);
-
-// Reverse array of u32s from i to j (i and j can wrap around)
-void ReverseArrayU32(u32 *array, u32 n_elements, u32 i, u32 j);
 
 i32 RoundNearest(f64 x);
 
