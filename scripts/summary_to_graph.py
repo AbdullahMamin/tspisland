58bcd652f60d5af7058add74a6aad3dce5734f52
Abdullah <aabdullahmamin@gmail.com>
2026-07-05 20:31:18 +0400

    Update to script

--- scripts/summary_to_graph.py
+++ scripts/summary_to_graph.py
@@ -7,19 +7,14 @@ assert(len(sys.argv) == 3)
 df = pd.read_csv(sys.argv[1])
 
 x = [i for i in range(len(df))]
-avg_fit = df['avg']
-stddev_fit = df['stddev']
 worst_fit = df['min']
 best_fit = df['max']
-avg_fit_up = [avg + 1.96*stddev for (avg, stddev) in zip(avg_fit, stddev_fit)]
-avg_fit_down = [avg - 1.96*stddev for (avg, stddev) in zip(avg_fit, stddev_fit)]
+avg_fit = df['avg']
 
 plt.title('Population summary')
 plt.xlabel('Generation')
-plt.plot(x, avg_fit_down, color='blue', linestyle='--')
-plt.plot(x, avg_fit, color='blue', label='avg fitness (95% conf)')
-plt.plot(x, avg_fit_up, color='blue', linestyle='--')
-plt.plot(x, best_fit, color='green', label='best fitness')
 plt.plot(x, worst_fit, color='red', label='worst fitness')
+plt.plot(x, best_fit, color='green', label='best fitness')
+plt.plot(x, avg_fit, color='blue', label='avg fitness (95% conf)')
 plt.legend()
 plt.savefig(sys.argv[2])
