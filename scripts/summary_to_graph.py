0866cd6203f978ca427bfd5379eabf9ab6b3a3bc
Abdullah <aabdullahmamin@gmail.com>
2026-07-12 17:04:31 +0400

    Not graphing minimum fitness because it is too sporadic

--- scripts/summary_to_graph.py
+++ scripts/summary_to_graph.py
@@ -7,13 +7,11 @@ assert(len(sys.argv) == 3)
 df = pd.read_csv(sys.argv[1])
 
 x = [i for i in range(len(df))]
-worst_fit = df['min']
 best_fit = df['max']
 avg_fit = df['avg']
 
 plt.title('Population summary')
 plt.xlabel('Generation')
-plt.plot(x, worst_fit, color='red', label='worst fitness')
 plt.plot(x, best_fit, color='green', label='best fitness')
 plt.plot(x, avg_fit, color='blue', label='avg fitness (95% conf)')
 plt.legend()
