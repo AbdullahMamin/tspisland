51a9c6440084f7b3f4bf8c3f37df4d95a569911c
Abdullah <aabdullahmamin@gmail.com>
2026-07-13 14:00:41 +0400

    Updated experiments

--- experiments.sh
+++ experiments.sh
@@ -1,14 +1,14 @@
-# A set of quick experiments on various test data. Outputs go to out/experiments
+# A quick experiment of the code. Modify as needed.
 echo "== Running experiments =="
 
 n_procs=4
 
 out_path="out/experiments"
-tsplib_path="data/tsplib"
+tspdata_path="data/tsplib"
 problem="berlin52"
 
 binary=out/bin/tspisland
-mkdir -p $out_path/$problem/population1000
-mpirun -np $n_procs $binary examples/quick_ring_population1000.toml $tsplib_path/$problem.tsp $out_path/$problem/population1000
+mkdir -p $out_path/$problem/test_run
+mpirun -np $n_procs $binary examples/quick_ring_population100.toml $tspdata_path/$problem.tsp $out_path/$problem/test_run
 
 echo "== Finished experiments! =="
